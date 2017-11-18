97947d35ed3f28869202b8916ab5ccb451f65562
mingfang25 <33434705+mingfang25@users.noreply.github.com>
2017-11-18 17:38:44 -0500

    Update tree.h

--- tree.h
+++ tree.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
 #include <algorithm>    // std::max
+using namespace std;
+#include <mutex>
+
 
 
 /// TODO: complete this implementation of a thread-safe (concurrent) AVL
@@ -10,6 +15,10 @@
 class tree
 {
 //This is for testing purposes. Do not access your Node directly.
+	size_t size = 0;
+	mutable std::mutex mtx;  
+
+
 public:
 	struct Node
 	{
@@ -17,6 +26,8 @@ public:
 			int value;
 			Node* left;
 			Node* right;
+			//Node* parent;
+			int height = 0;
 	};
 	Node* head;
 
@@ -28,42 +39,118 @@ public:
 	{}
 
 
-	bool inserthelper(int data){
-		if(head == NULL)
+	Node* inserthelper(Node* root, int data){
+		//printf(" helper %d\n",data);
+		if(root == NULL)
 		{
-			head = new Node;
-			head -> value = *data;
-			head -> left = NULL;
-			head -> right = NULL;
+				//*result = true;
+				
+				//result = true;
+				size++;
+				root = new Node;
+				root->value = data;
+				root->left = NULL;
+				root->right = NULL;
+				root->height = 0;
+			
+			return root;
 		}
-		else if(key< head->value)
+		else if(data < root->value)
 		{
-			head->left = insert((int*)(head->left->value), results, num);
-			head = balance(head);
-		}
-		else if(key >= head->value)
-		{
-			head->right = insert(head->right->value, key);
-			head = balance(head);
-		}
+			root->left = inserthelper(root->left, data);
+			//printf("call\n");
 
+			
+		}
+		else if(data > root->value)
+		{
+			root->right = inserthelper(root->right,data);
+			//printf("call\n" );
+
+
+			
+		}
+		else if(root->value == data)
+		{
+			//results = false;
+			return root;
+		}
+		//int height = findHeight(root);
+
+		return root= balance(root);
 
 	}
-	/// insert /num/ values from /data/ array into the tree, and return the
-	/// success/failure of each insert in /results/ array.
-	void insert(int* data, bool* results, int num)
+
+
+	Node *balance(Node *root)
 	{
-	
-		for(int i=0; i<num; i++){
-			int insertvalue=data[i];
-			bool retval=inserthelper(insertvalue);
-			results[i]=retval;
-
-		}
+		int balance = balanceFactor(root);
+	    if (balance == 2)
+	    {
+	        if (balanceFactor (root->left) > 0)
+	        {
+	            root = llrotation(root);//left left
+	        }
+	        else
+	        {
+	            root = lrrotation(root);//left right
+	        }
+	    }
+	    else if (balance == -2)
+	    {
+	        if (balanceFactor (root->right) > 0)
+	            root = rlrotation (root);//right left
+	        else
+	            root = rrrotation (root);// right right
+	    }
+	    return root;
 	}
+
+
+
+	Node *llrotation(Node *root)
+	{
+		Node *a = root->left;
+		root->left = a->right;
+		a->right = root;
+		return a;
+	}
+
+
+	Node *rrrotation(Node *root)
+	{
+		Node *a = root->right;
+		root->right = a->left;
+		a->left = root;
+
+		return a;
+	}
+
+	Node *lrrotation(Node *root)
+	{
+		Node *a = root->left;
+		root->left = rrrotation(a);
+		return llrotation(root);
+	}
+
+	Node *rlrotation(Node *root)
+	{
+		Node *a = root->right;
+		root->right = llrotation(a);
+		return rrrotation(root);
+	}
+
 
 	int findHeight(Node* node)
 	{
+	// 	if(node == NULL)
+// 		{
+// 			return -1;
+// 		}
+// 		else
+// 		{
+// 			return node->height;
+// 		}
 		int height = 0;
 		if(node != NULL)
 		{
@@ -75,19 +162,178 @@ public:
 		return height;
 	}
 
-	Node* balance(Node* head)
+	int balanceFactor(Node* head)
 	{
 		int balanceFactor = findHeight(head->left)-findHeight(head->right);
-		return head;
+		//printf("BF: %d",balanceFactor);
+		return balanceFactor;
 	}
+
+
+	/// insert /num/ values from /data/ array into the tree, and return the
+	/// success/failure of each insert in /results/ array.
+	void insert(int* data, bool* results, int num)
+	{
+		// printf("insert start: ");
+		//inserthelper(head, data);
+		// printf("insert  %d\n", data[0]);
+		//mtx.lock();
+		// for(int i=0; i<num; i++)
+// 		{
+			mtx.lock();
+			//int insertvalue=data[i];
+			//bool retval=inserthelper(insertvalue);
+			//printf("insert  %d\n", data[0]);
+			head = inserthelper(head, data[0]);
+			//printf("result  %s\n", results[i]);
+		
+			results[0]=true;
+			//display(head, 1);
+			//printf("insert over\n");
+			mtx.unlock();
+			//printf("result  %d\n", getSize());
+// 		}
+		//mtx.unlock();
+		//postorder(head);
+	}
+
+
+
+	void display(Node *ptr, int level)
+	{
+	    int i;
+	    if (ptr!=NULL)
+	    {
+	        display(ptr->right, level + 1);
+	        printf("\n");
+	        if (ptr == head)
+	        cout<<"Root -> ";
+	        for (i = 0; i < level && ptr != head; i++)
+	            cout<<"        ";
+	        cout<<ptr->value;
+	        display(ptr->left, level + 1);
+	    }
+	}
+
+	Node *removehelper(Node *root, int data)
+	{
+		//printf(" helper %d\n",data);
+		if(root == NULL)
+		{
+			
+			return root;
+		}
+		else if(data < root->value)
+		{
+			root->left = removehelper(root->left, data);
+			//printf("call\n");
+
+			
+		}
+		else if(data > root->value)
+		{
+			root->right = removehelper(root->right,data);
+			//printf("call\n" );
+
+
+			
+		}
+		else if(root->value == data)
+		{
+			size--;
+			if(root->left == NULL)
+			{
+				Node *node = root->left;
+				delete root;
+				return node;
+			}
+			else if(root->right == NULL)
+			{
+				Node *node = root->right;
+				delete root;
+				return node;
+			}
+			else if (root->left != NULL && root->right != NULL)
+			{
+				 Node* temp = root->right;
+				 while(temp -> left != NULL)
+				 {
+				 	temp = temp->left;
+				 }
+				 
+           		 root->value = temp->value;
+            	 root->right = removehelper(root->right, temp->value);
+			}
+			
+			root = balance(root);
+			//results = false;
+			//return root;
+		}
+		
+		
+		//int height = findHeight(root);
+
+		return root;//node= balance(root);
+
+	}
+	
+
+	
+
 
 	/// remove *data* from the list if it was present; return true if the data
 	/// was removed successfully.
 	void remove(int* data, bool* results, int num)
-	{}
+	{
+		mtx.lock();
+		// for(int i=0; i<num; i++)
+// 		{
+			//mtx.lock();
+			//printf("remove value: %d\n", data[i]);
+			//display(head,1);
+			head = removehelper(head, data[0]);
+			//printf("after remove\n");
+			 //display(head,1);
+			//mtx.unlock();
+		//}
+		mtx.unlock();
+	}
 	/// return true if *data* is present in the list, false otherwise
-	void lookup(int* data, bool* results, int num) const
-	{}
+	void lookup(int* data, bool* results, int num) //const
+	{
+		mtx.lock();
+		// for(int i=0; i<num; i++)
+// 		{
+			//results[i] = false;
+			//mtx.lock();
+			results[0] = lookupHelper(head, data[0]);
+			//mtx.unlock();
+		//}
+		mtx.unlock();
+		
+		
+
+	}
+	
+	bool lookupHelper(Node *head, int data)
+	{
+		
+			Node* a = head;
+			while(a!=NULL){
+				if(a->value < data){
+					a = a->right;
+				}
+				else if(a->value > data){
+					a = a->left;
+				}
+				else if(a->value == data){
+					//printf("success\n");
+					return true;
+				}
+			}
+			//printf("false\n");
+			return false;
+	}
 
 	//The following are not tested by the given tester but are required for grading
 	//No locks are required for these.
@@ -95,14 +341,18 @@ public:
 	//Total number of elements in the tree
 	size_t getSize() const
 	{
-		return 0;
+		return size;
 	}
 
 	int getElement(size_t idx) const
 	{
-		return getElement(0, head);
+
+		return getElement(idx, head);
+
+  
 	}
 
+	
 
 	//These functions need to exist, they do not need to do anyting
 	size_t getBucketSize() const
@@ -115,11 +365,44 @@ public:
 	}
 
 
+int getnode(Node *head)
+{
+	 if (head=NULL)
+	 {
+		  return 0;
+	 }
+	 else
+	 {
+	  	return 1+ getnode(root->left) + getnode(root->right);
+	 }
+}
 
 private:
 	//Returns the idx'th element from the tree assuming an inorder traversal.
 	int getElement(size_t idx, Node* at) const
 	{
+		int result = 0;
+		//return 0;
+		if(head == NULL)
+		{
+			return -1;
+		}
+		if(getNode(head->left)<idx)
+		{
+			
+		}
+		// while(at->left!=NULL)
+// 		{
+// 			getElement(idx, head->left);
+// 		
+// 		}
+// 		while(idx!=0)
+// 		{
+// 			result = at->value;
+// 			//To do: traverse inorder
+// 			idx--;
+// 		}
 		return 0;
+		
 	}
 };
